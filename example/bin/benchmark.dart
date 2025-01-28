f9a88be20fee87ab76b89d2a9ec3dd533311f290
Victor Dunaev <nashol@yandex-team.ru>
2025-01-29 00:12:56 +0500

    Write README

--- example/bin/benchmark.dart
+++ example/bin/benchmark.dart
@@ -1,5 +1,4 @@
 import 'package:ansi_escape_codes/ansi_escape_codes.dart';
-import 'package:decimal2/decimal2.dart';
 import 'package:example/benchmark.dart';
 
 void printUsage() {
@@ -43,31 +42,6 @@ void printUsage() {
 }
 
 void main(List<String> arguments) {
-  final v = Decimal(1000000000000000000, shiftRight: 18);
-
-  final sw = Stopwatch()..start();
-  for (var i = 0; i < 10000000; i++) {
-    v.toString();
-  }
-  sw.stop();
-  print(sw.elapsed); // 0:00:02.406469
-
-  sw
-    ..reset()
-    ..start();
-  for (var i = 0; i < 10000000; i++) {
-    // Это оптимизация, так как мы много раз используем одно и тоже значение.
-    // Оптимизация выполнится только один раз. Остальное время мы вхолостую
-    // будем запускать метод. В данном случае можно было бы выполнить
-    // оптимизацию один раз до цикла.
-    v.optimize();
-    v.toString();
-  }
-  sw.stop();
-  print(sw.elapsed); // 00:00:00.061205
-
-  return;
-
   if (arguments.isEmpty || arguments.isNotEmpty && arguments[0] == '--help') {
     printUsage();
     return;
